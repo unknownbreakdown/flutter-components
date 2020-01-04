1b1a2d2e2b7e340aaf3377119fc76aa65dd4f277
unknownbreakdown <unknownbreakdown@gmail.com>
2020-01-03 20:07:43 -0500

    Image y Fadein Image

--- lib/src/pages/card.dart
+++ lib/src/pages/card.dart
@@ -10,13 +10,15 @@ class CardPage extends StatelessWidget {
       body: ListView(
         padding: EdgeInsets.all(10.0),
         children: <Widget>[
-          _getCardType1(),
+          _buildCardType1(),
+          SizedBox(height: 10.0),
+          _buildCardType2(),
         ],
       ),
     );
   }
 
-  Widget _getCardType1() {
+  Widget _buildCardType1() {
     return Card(
       child: Column(
         children: <Widget>[
@@ -37,6 +39,27 @@ class CardPage extends StatelessWidget {
                 onPressed: () {},
               ),
             ],
+          )
+        ],
+      ),
+    );
+  }
+
+  _buildCardType2() {
+    return Card(
+      child: Column(
+        children: <Widget>[
+          FadeInImage(
+            placeholder: AssetImage('images/loading.gif'),
+            image: NetworkImage(
+                'https://pro2-bar-s3-cdn-cf5.myportfolio.com/f1b116d8e4a1395c6304122d31ec59fb/601b545a-778a-4222-8949-5535a3d9ee36_rw_1920.jpg?h=08ded2642ba54a3d2556d5e30075c0ba'),
+            fadeInDuration: Duration(milliseconds: 300),
+            height: 300.0,
+            fit: BoxFit.cover,
+          ),
+          Container(
+            padding: EdgeInsets.all(10.0),
+            child: Text("Sample description text"),
           )
         ],
       ),
