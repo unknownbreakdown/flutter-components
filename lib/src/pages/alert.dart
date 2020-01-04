2f0822fae26712662a7d2ac37457476be8a1f64d
unknownbreakdown <unknownbreakdown@gmail.com>
2020-01-04 18:57:55 -0500

    Alert Widget

--- lib/src/pages/alert.dart
+++ lib/src/pages/alert.dart
@@ -7,6 +7,55 @@ class AlertPage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Alert Page"),
       ),
+      body: Center(
+        child: RaisedButton(
+          color: Colors.blue,
+          textColor: Colors.white,
+          shape: StadiumBorder(),
+          child: Text("Show alert"),
+          onPressed: () {
+            return _showAlert(context);
+          },
+        ),
+      ),
+    );
+  }
+
+  void _showAlert(BuildContext context) {
+    showDialog(
+      context: context,
+      barrierDismissible: true,
+      builder: (context) {
+        return AlertDialog(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(5.0),
+          ),
+          title: Text("Title"),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Text("Alert description"),
+              FlutterLogo(
+                size: 50.0,
+              ),
+            ],
+          ),
+          actions: <Widget>[
+            FlatButton(
+              child: Text("Cancel"),
+              onPressed: () {
+                return Navigator.of(context).pop();
+              },
+            ),
+            FlatButton(
+              child: Text("Ok"),
+              onPressed: () {
+                return Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
     );
   }
 }
