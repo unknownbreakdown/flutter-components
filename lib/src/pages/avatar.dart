75ec61061ce308f96d99581afc2618a69fe31b9c
unknownbreakdown <unknownbreakdown@gmail.com>
2020-01-04 19:26:35 -0500

    CircleAvatar Widget

--- lib/src/pages/avatar.dart
+++ lib/src/pages/avatar.dart
@@ -6,6 +6,21 @@ class AvatarPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text("Avatar Page"),
+        actions: <Widget>[
+          Container(
+            padding: EdgeInsets.all(7.0),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage('https://pm1.narvii.com/6588/aa21e53570131df6cc540dda3b4ef77489147209_128.jpg'),
+              radius: 21.0,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(right: 10.0),
+            child: CircleAvatar(
+              child: Text("CBE"),
+            ),
+          )
+        ],
       ),
     );
   }
