65796d1420b17b1073b0febabc7ba188e3dddd4f
unknownbreakdown <unknownbreakdown@gmail.com>
2020-01-09 18:17:27 -0500

    Infinite Scroll with Futures

--- lib/src/pages/list.dart
+++ lib/src/pages/list.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 class ListPage extends StatefulWidget {
@@ -9,6 +11,7 @@ class _ListPageState extends State<ListPage> {
   List<int> _numbers = List();
   int _lastItem = 0;
   ScrollController _scrollController = ScrollController();
+  bool _isLoading = false;
 
   @override
   void initState() {
@@ -16,20 +19,32 @@ class _ListPageState extends State<ListPage> {
     _addItems();
 
     _scrollController.addListener(() {
-      if (_scrollController.position.pixels == _scrollController.position.maxScrollExtent) {
-        _addItems();
+      if (_scrollController.position.pixels ==
+          _scrollController.position.maxScrollExtent) {
+        //_addItems();
+        _fetchData();
       }
     });
   }
 
   @override
+  void dispose() {
+    super.dispose();
+    _scrollController.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("List & Scroll"),
-      ),
-      body: _buildList(),
-    );
+        appBar: AppBar(
+          title: Text("List & Scroll"),
+        ),
+        body: Stack(
+          children: <Widget>[
+            _buildList(),
+            _buildLoading(),
+          ],
+        ));
   }
 
   _buildList() {
@@ -42,6 +57,7 @@ class _ListPageState extends State<ListPage> {
         return FadeInImage(
           placeholder: AssetImage('images/loading.gif'),
           image: NetworkImage('https://picsum.photos/id/$imageNumber/500/300'),
+          height: 220.0,
         );
       },
     );
@@ -54,5 +70,33 @@ class _ListPageState extends State<ListPage> {
     }
 
     setState(() {});
+  }
+
+  Future<Null> _fetchData() async {
+    _isLoading = true;
+    setState(() {});
+    Duration duration = Duration(seconds: 3);
+
+    return new Timer(duration, _httpResponse);
+  }
+
+  void _httpResponse() {
+    _isLoading = false;
+    _addItems();
+    _scrollController.animateTo(
+      _scrollController.position.pixels + 30,
+      curve: Curves.fastOutSlowIn,
+      duration: Duration(milliseconds: 300)
+    );
+  }
+
+  Widget _buildLoading() {
+    if (_isLoading) {
+      return Center(
+        child: CircularProgressIndicator(),
+      );
+    } else {
+      return Container();
+    }
   }
 }
